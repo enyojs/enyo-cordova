c6803e6271f6647340c3f0a492cf11160984af67
Jason Robitaille <jason.robitaille@lge.com>
2013-10-01 09:40:47 -0500

    Changed the tv module loader to ensure tv modules are loaded after Cordova is in memory, but before DOMContentLoaded. Enyo-DCO-1.1-Signed-off-by: Jason Robitaille <jason.robitaille@lge.com>

--- tv/cordova-tv-loader.js
+++ tv/cordova-tv-loader.js
@@ -2,26 +2,37 @@
 Loads the external Cordova TV modules for webOS TV platform.
 */
 //* @protected
-(function(ctx){
-	if(ctx.cordova || ctx.PhoneGap) {
-		if(window.PalmSystem && (!enyo.platform.webos || enyo.platform.webos>3)) {
-			var cordovaTVVersion = ctx.useCordovaTVVersion || "3.0";
-			var tvModules = [
-				"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/advertisement.js",
-				"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/billing.js",
-				"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/broadcast.js",
-				"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/camera.js",
-				"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/deviceinfo.js",
-				"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/drmagent.js",
-				"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/mrcu.js",
-				"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/push.js",
-				"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/upnp.js"
-			];
-			enyo.depends.apply(enyo, tvModules);
-		} else {
-			enyo.warn("Platform does not support Cordova TV modules, skipping");
-		}
+(function(){
+	if(window.PalmSystem && (!enyo.platform.webos || enyo.platform.webos>3)) {
+		var cordovaTVVersion = ctx.useCordovaTVVersion || "3.0";
+		var tvModules = [
+			"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/advertisement.js",
+			"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/billing.js",
+			"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/broadcast.js",
+			"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/camera.js",
+			"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/deviceinfo.js",
+			"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/drmagent.js",
+			"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/mrcu.js",
+			"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/push.js",
+			"/usr/palm/frameworks/cordova-tv/" + cordovaTVVersion + "/upnp.js"
+		];
+		
+		/*
+			Inject an inline js text node to check for existence of Cordova as we cannot have a guaranteed
+			accurate check at the time cordova-tv-loader.js runs. This is because enyo-cordova dynamically
+			adds the cordova js at runtime, so we must check after Cordova is in memory, and only then can we
+			add our TV libraries. This allows us to add the TV modules after Cordova but before DOMContentLoaded.
+		*/		
+		var inline = document.createElement("script");
+		var inject = "if(window.cordova || ctx.PhoneGap) {\n" +
+				"enyo.depends.apply(enyo, " + enyo.json.stringify(tvModules) + ");\n" +
+				"} else {\n" +
+				"enyo.warn(\"Cordova not found, ignoring tv modules\");\n" +
+				"}";
+		inline.appendChild(document.createTextNode(inject);
+		document.getElementsByTagName("head")[0].appendChild(inline);
+		
 	} else {
-		enyo.warn("Cordova not found, ignoring tv modules");
+		enyo.warn("Platform does not support Cordova TV modules, skipping");
 	}
-})(window);
+})();
