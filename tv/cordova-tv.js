ff2bd408babb290f8767d6c159714b4bd1b16914
art.dahm <art.dahm@lge.com>
2014-07-22 14:53:14 -0700

    ENYO-15: Complete JSDoc3 conversion on enyo-cordova

--- tv/cordova-tv.js
+++ tv/cordova-tv.js
@@ -1,11 +1,10 @@
 /**
-	Loads the external Cordova TV modules for webOS TV platform.
-*/
-//* @protected
-(function(){
-	if(window.PalmSystem && (!enyo.platform.webos || enyo.platform.webos>3)) {
+ * Loads the external Cordova TV modules for webOS TV platform.
+ */
+(function(enyo, scope){
+	if(scope.PalmSystem && (!enyo.platform.webos || enyo.platform.webos>3)) {
 		enyo.load("$lib/enyo-cordova/tv/tv-module-loader.js");
 	} else {
 		enyo.warn("Platform does not support Cordova TV modules, skipping");
 	}
-})();
+})(enyo, window);
