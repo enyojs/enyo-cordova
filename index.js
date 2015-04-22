432dbe16cca2489890db90afcbe1b9073f5a6c03
Derek Anderson <derek.anderson@lge.com>
2015-04-22 15:08:22 -0700

    working and loaded cordova

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+window.PLATFORM_VERSION_BUILD_LABEL = 'webos';
+
 var
+	r = require('cordova/require');
+	window.require = r.require;
+	window.define = r.define;
+
+var
+	webos = require('cordova/webos'),
+	service = require('cordova/webos/service'),
 	cordova = require('cordova');
 
-module.exports = cordova;
+window.cordova = cordova;
+
+webos.bootstrap();
+
+module.exports = webos;
